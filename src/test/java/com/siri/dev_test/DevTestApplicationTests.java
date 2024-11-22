af5b8da6ca0afb20877a0fc5b3f91cc0c2ef5dd6
manjula <manjula.ranathunga.sl@gmail.com>
2024-11-22 01:11:32 +0100

    init commit

---
+++ src/test/java/com/siri/dev_test/DevTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siri.dev_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
